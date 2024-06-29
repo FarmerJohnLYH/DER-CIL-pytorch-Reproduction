32aaa4f0459b39b8a273158105f615a4f0f9d988
YuanHao Liu <yuanhaoliu@YuanHaodeMacBook-Air.local>
2024-06-30 00:39:32 +0800

    增加 readme 内容，修改了不合适的文件

--- README.md
+++ README.md
@@ -11,6 +11,18 @@
 ## 运行实验 
 * 切换到相应的目录并运行以下命令
 ```
-sh scripts/lyh.sh
-sh scripts/lyh_noder.sh
+sh scripts/three_exps.sh
+sh scripts/three_exps_without_der.sh
 ```
+第一个脚本包括“Base-0,Inc-5”等三种增量学习策略。
+第二个脚本是不使用 der 的运行脚本，用于测试 der 对性能的提升。
+
+## 主要内容
+1. **Docx_with_Annotations**
+包含撰写的实验报告和PPT。
+2. **Charts**
+包含了在 PPT 中用到的大部分原始的数据的表格。
+3. **configs**
+包含参数配置。
+4. **scripts**
+复现论文所用的脚本。
--- scripts/three_exps_without_der.sh
+++ scripts/three_exps_without_der.sh
@@ -13,30 +13,30 @@ expid='B0S10_noder'
     --force 
 
 
-# name='B0S5_noder'
-# comments='None'
-# expid='B0S5_noder'
-#     python -m main train with "./configs/${expid}.yaml" \
-#     exp.name="${name}" \
-#     exp.savedir="./logs/" \
-#     exp.ckptdir="./logs/" \
-#     exp.tensorboard_dir="./tensorboard/" \
-#     exp.debug=True \
-#     --name="${name}" \
-#     -D \
-#     -p \
-#     --force 
+name='B0S5_noder'
+comments='None'
+expid='B0S5_noder'
+    python -m main train with "./configs/${expid}.yaml" \
+    exp.name="${name}" \
+    exp.savedir="./logs/" \
+    exp.ckptdir="./logs/" \
+    exp.tensorboard_dir="./tensorboard/" \
+    exp.debug=True \
+    --name="${name}" \
+    -D \
+    -p \
+    --force 
 
-# name='B50S10_noder'
-# comments='None'
-# expid='B50S10_noder'
-#     python -m main train with "./configs/${expid}.yaml" \
-#     exp.name="${name}" \
-#     exp.savedir="./logs/" \
-#     exp.ckptdir="./logs/" \
-#     exp.tensorboard_dir="./tensorboard/" \
-#     exp.debug=True \
-#     --name="${name}" \
-#     -D \
-#     -p \
-#     --force 
+name='B50S10_noder'
+comments='None'
+expid='B50S10_noder'
+    python -m main train with "./configs/${expid}.yaml" \
+    exp.name="${name}" \
+    exp.savedir="./logs/" \
+    exp.ckptdir="./logs/" \
+    exp.tensorboard_dir="./tensorboard/" \
+    exp.debug=True \
+    --name="${name}" \
+    -D \
+    -p \
+    --force 
